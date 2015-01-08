7f882539942b680860c0dad2f77a72675b692092
markrose <markrose@motorola.com>
2015-01-07 19:53:01 -0800

    added g+ login

--- app/src/main/java/us/scoreme/locationpicker/WifiBroadCastReceiver.java
+++ app/src/main/java/us/scoreme/locationpicker/WifiBroadCastReceiver.java
@@ -1,8 +1,10 @@
 package us.scoreme.locationpicker;
 
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -19,10 +21,19 @@ public class WifiBroadCastReceiver extends BroadcastReceiver {
         Log.e("on receiver", "wifi change detected:"+i+e);
         Toast.makeText(context, "wifi change detected:"+i+e, Toast.LENGTH_SHORT).show();
 
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(context)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("New wifi change")
+                        .setContentText(i+e);
+
+        NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        mNotificationManager.notify(1, mBuilder.build());
+
         Context appContext = context.getApplicationContext();
 
         Intent newIntent = new Intent(appContext, secondActivity.class);
         newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        appContext.startActivity(newIntent);
+        //appContext.startActivity(newIntent);
     }
 }
--- app/src/main/java/us/scoreme/locationpicker/secondActivity.java
+++ app/src/main/java/us/scoreme/locationpicker/secondActivity.java
@@ -3,16 +3,26 @@ package us.scoreme.locationpicker;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
+
+import java.io.BufferedWriter;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class secondActivity extends Activity implements httpReply {
 
     private PendingIntent pendingIntent;
 
+
     @Override
     public void updateActivity(String result){
         Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+        appendLog(result);
     }
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,61 @@ public class secondActivity extends Activity implements httpReply {
         String a="43";
         new httpRequest(this).execute("http://www.scoreme.us/a.php?r=2",a,"apple");
     }
+
+    public void viewLog(View view){
+        FileInputStream fis;
+        final StringBuffer storedString = new StringBuffer();
+
+        try {
+            fis = openFileInput("log1.txt");
+            DataInputStream dataIO = new DataInputStream(fis);
+            String strLine = null;
+
+            if ((strLine = dataIO.readLine()) != null) {
+                storedString.append(strLine);
+            }
+
+            dataIO.close();
+            fis.close();
+            String S=storedString.toString();
+            Log.e("test",S);
+        }
+        catch  (Exception e) {
+        }
+    }
+
+    public void appendLog(String text)
+    {
+        String filePath = getFilesDir().getPath().toString() + "/log1.txt";
+        File logFile = new File(filePath);
+        if (!logFile.exists())
+        {
+            try
+            {
+                logFile.createNewFile();
+            }
+            catch (IOException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+        try
+        {
+            //BufferedWriter for performance, true to set append to file flag
+
+            BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true));
+            buf.append(text);
+            buf.newLine();
+            buf.close();
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 
 }
 
