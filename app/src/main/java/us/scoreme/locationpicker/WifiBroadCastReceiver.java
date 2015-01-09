3bbbe05162b714e302da0f6be6c9af5d2da1f4b4
markrose <markrose@motorola.com>
2015-01-08 17:47:13 -0800

    added g+ login

--- app/src/main/java/us/scoreme/locationpicker/WifiBroadCastReceiver.java
+++ app/src/main/java/us/scoreme/locationpicker/WifiBroadCastReceiver.java
@@ -1,39 +1,58 @@
 package us.scoreme.locationpicker;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.net.URLEncoder;
 
 public class WifiBroadCastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        String id = intent.getStringExtra("id");
+        Context appContext = context.getApplicationContext();
 
         String i=intent.toString();
         String e=intent.getExtras().toString();
+        String link="http://www.scoreme.us/a.php?view=1";
 
         Log.e("on receiver", "wifi change detected:"+i+e);
         Toast.makeText(context, "wifi change detected:"+i+e, Toast.LENGTH_SHORT).show();
 
+
+        Intent resultIntent = new Intent(Intent.ACTION_VIEW);
+        resultIntent.setData(Uri.parse(link));
+
+        PendingIntent pending = PendingIntent.getActivity(appContext, 0, resultIntent, PendingIntent.FLAG_ONE_SHOT);
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
-                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setSmallIcon(R.drawable.myicon)
                         .setContentTitle("New wifi change")
-                        .setContentText(i+e);
+                        .setContentText("View latest details...")
+                        .setContentIntent(pending);
 
         NotificationManager mNotificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
         mNotificationManager.notify(1, mBuilder.build());
 
-        Context appContext = context.getApplicationContext();
+        String url="http://www.scoreme.us/a.php?data=1&event=";
+        Intent myServiceIntent = new Intent(appContext, httpRequest2.class);
+        String data=i+e;
+        String encodedData= URLEncoder.encode(data);
 
-        Intent newIntent = new Intent(appContext, secondActivity.class);
-        newIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        //appContext.startActivity(newIntent);
+        myServiceIntent.putExtra("STRING_I_NEED",encodedData);
+        myServiceIntent.putExtra("URL", url);
+        appContext.startService(myServiceIntent);
+
     }
+
+
 }
